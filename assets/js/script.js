ef9157aaa2d418a166a3d4e250714d5555564e4b
Emese92 <gregor.emese@gmail.com>
2022-02-09 18:15:14 +0000

    Tidy upp and replace pictures with icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,105 @@
+const weapons = [{
+  id: 'rock',
+  name: 'Rock',
+  winsOver: ['scissors', 'lizard'],
+  iconClassName: 'fa-hand-rock',
+},
+{
+  id: 'paper',
+  name: 'Paper',
+  winsOver: ['scissors', 'lizard'],
+  iconClassName: 'fa-hand-paper',
+},
+{
+  id: 'scissors',
+  name: 'Scissors',
+  winsOver: ['scissors', 'lizard'],
+  iconClassName: 'fa-hand-scissors',
+},
+{
+  id: 'lizard',
+  name: 'Lizard',
+  winsOver: ['scissors', 'lizard'],
+  iconClassName: 'fa-hand-lizard',
+},
+{
+  id: 'spock',
+  name: 'Spock',
+  winsOver: ['scissors', 'lizard'],
+  iconClassName: 'fa-hand-spock',
+}
+];
+
+
 //Creating variables
 let score = 0;
 let rounds = 0;
 const scoreSpan = document.getElementById("score-number");
 const roundsSpan = document.getElementById("round-number");
 const message = document.getElementById("message");
-var computerImage = document.querySelector("#computer-image");
-var playerImage = document.querySelector("#player-image");
+var computerIcon = document.querySelector("#computer-icon");
+var playerIcon = document.querySelector("#player-icon");
 
-const weapons = [{
-    id: 'rock',
-    name: 'Rock',
-    winsOver: ['scissors', 'lizard']
-  },
-  {
-    id: 'paper',
-    name: 'Paper',
-    winsOver: ['scissors', 'lizard']
-  },
-  {
-    id: 'scissors',
-    name: 'Scissors',
-    winsOver: ['scissors', 'lizard']
-  },
-  {
-    id: 'lizard',
-    name: 'Lizard',
-    winsOver: ['scissors', 'lizard']
-  },
-  {
-    id: 'spock',
-    name: 'Spock',
-    winsOver: ['scissors', 'lizard']
-  }
-];
 
 //Computer choice
 function getComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 5);
-  return weapons[randomNumber].id;
+const randomNumber = Math.floor(Math.random() * 5);
+return weapons[randomNumber].id;
 
 }
 
 //Counters and result
-
 function draw() {
-  rounds++;
-  roundsSpan.innerHTML = rounds;
-  message.textContent = "Its a draw! Try again!";
+rounds++;
+roundsSpan.innerHTML = rounds;
+message.textContent = "Its a draw! Try again!";
 }
 
 function win() {
-  score++;
-  scoreSpan.innerHTML = score;
-  rounds++;
-  roundsSpan.innerHTML = rounds;
-  message.textContent = "Congratulations, you won!";
+score++;
+scoreSpan.innerHTML = score;
+rounds++;
+roundsSpan.innerHTML = rounds;
+message.textContent = "Congratulations, you won!";
 }
 
 function lose() {
-  rounds++;
-  roundsSpan.innerHTML = rounds;
-  message.textContent = "You lost! Better luck next time!";
+rounds++;
+roundsSpan.innerHTML = rounds;
+message.textContent = "You lost! Better luck next time!";
 }
 
 //Player choice
 
-function buttons(playerChoiceId) {
-  const computerChoiceId = getComputerChoice();
+function onWeaponClick(playerChoiceId) {
+const computerChoiceId = getComputerChoice();
 
-  const playerChoiceWinsOver = weapons.find(eachOption => eachOption.id === playerChoiceId).winsOver;
-  if (playerChoiceId === computerChoiceId) {
-    draw();
-  } else if (playerChoiceWinsOver.includes(computerChoiceId)) {
-    // Player wins
-    win();
-  } else {
-    //Computer wins
-    lose();
-  }
+const playerChoiceData = weapons.find(eachOption => eachOption.id === playerChoiceId);
+const computerChoiceData = weapons.find(eachOption => eachOption.id === computerChoiceId);
 
-  //Return pictures
-  playerImage.src = `./assets/images/${playerChoiceId}.jpg`;
-  computerImage.src = `./assets/images/${computerChoiceId}.jpg`;
+// Update icons
+computerIcon.className = `fas ${computerChoiceData.iconClassName}`;
+playerIcon.className = `fas ${playerChoiceData.iconClassName}`;
+
+if (playerChoiceId === computerChoiceId) {
+  draw();
+} else if (playerChoiceData.winsOver.includes(computerChoiceId)) {
+  // Player wins
+  win();
+} else {
+  //Computer wins
+  lose();
+}
 }
 
 
 function addClickListenersToOptions() {
-  weapons.forEach(eachOption => {
-    const id = eachOption.id;
-    document.getElementById(id).addEventListener('click', function() {
-      buttons(id);
-    });
+weapons.forEach(eachOption => {
+  const id = eachOption.id;
+  document.getElementById(id).addEventListener('click', function() {
+    onWeaponClick(id);
   });
+});
 }
 
 
